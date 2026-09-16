3ff939f810db61fb192b00ac9427a8482cc365ae
Nicolas Lopez Fernandez <nicolas.lopezfernandez1@usp.ceu.es>
2026-09-16 23:31:04 +0200

    final2

--- PantallaAjustes.java
+++ PantallaAjustes.java
@@ -1,6 +1,7 @@
 public class PantallaAjustes {
     public void mostrar() {
         ConfiguracionInterfaz config = ConfiguracionInterfaz.getInstancia();
+        System.out.println("Pantalla de ajustes:");
         System.out.println("Idioma actual: " + config.getIdioma());
         System.out.println("Tamaño de fuente: " + config.getTamañoFuente());
     }
